5e65921ce50353c7240fb117ed74761baa2bc29b
harshbhane0 <harshbhange123@gmail.com>
2024-04-06 18:52:17 +0530

    "Import 'NextFunction' from express and add it to the app.get callback; add user counter to app.get response"
    Explanation:
    
    * The code import "NextFunction" from express and added it to the app.get callback.
    * A variable "user" is initialized and incremented in the app.get callback.
    * The value of "user" is logged to the console and included in the app.get response.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { ConnectDB } from "./database";
 import { routerG } from "./routes";
@@ -7,9 +7,14 @@ const app = express();
 const port = 3000;
 dotenv.config();
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
-  res.json({ health: "running", data }).status(200);
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  user + 1;
+  console.log(`User Connected : ${user}`);
+  next();
+
+  res.json({ health: "running", data, user }).status(200);
 });
+let user = 0;
 app.use("/api/v1", routerG);
 ConnectDB();
 app.listen(port, () => {
