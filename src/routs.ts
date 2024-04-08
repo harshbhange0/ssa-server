10a6f5acf5fc7e125c2296faf180a117d20a8400
harshbhane0 <harshbhange123@gmail.com>
2024-04-08 17:08:31 +0530

    "Add quiz object to routes data with CRUD operations for quiz and question"
    This commit adds a new object "quiz" to the "data" array in the "routs.ts" file. The "quiz" object contains a "preUrl" property set to "quiz/", an empty "solved" object, and an array of "unsolved" routes for CRUD operations related to quizzes and questions. The "unsolved" array includes routes for creating a quiz, creating a question, getting all quizzes, getting all admin quizzes, updating a quiz, and updating a question.

--- src/routs.ts
+++ src/routs.ts
@@ -8,4 +8,37 @@ export const data = [
       verify: { verify: "/verify/:id", method: "get" },
     },
   },
+  {
+    quiz: {
+      preUrl: "quiz/",
+      solved: {},
+      unsolved: [
+        {
+          create_quiz: "create/quiz",
+          method: "post",
+        },
+        {
+          create_question: "create/question",
+          method: "post",
+        },
+        {
+          get_all_quiz: "all/quiz",
+          method: "get",
+        },
+        {
+          get_all_admin_quiz: "all/admin/quiz/:adminId",
+          method: "get",
+        },
+        {
+          update_quiz: "update/quiz/:quizId",
+          method: "put",
+        },
+
+        {
+          update_question: "update/question/:questionId",
+          method: "put",
+        },
+      ],
+    },
+  },
 ];
