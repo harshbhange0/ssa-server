cd760b3efabc8bf9f5035681e071877770dcbe7c
harshbhane0 <harshbhange123@gmail.com>
2024-04-08 17:13:44 +0530

    "Replace user counter and response handling with ApiResponse helper function"
    The commit removes the use of a global variable `user` to count the number of users and replaces the response handling with a new helper function `ApiResponse` which is imported from "./utils/helper". This function is used to create a consistent response format for the application.

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 import { ConnectDB } from "./database";
 import { routerG } from "./routes";
 import { data } from "./routs";
+import { ApiResponse } from "./utils/helper";
 const app = express();
 const port = 3000;
 dotenv.config();
 app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
-  user = user + 1;
-  console.log(`User Connected : ${user}`);
-  res.json({ health: "running", data, user }).status(200);
+  return ApiResponse({ res, data, msg: "server  is running", code: 200 });
 });
 let user = 0;
 app.use("/api/v1", routerG);
