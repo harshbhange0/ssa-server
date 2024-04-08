1a831d9746e826fe66e80670877d3a92db618980
harshbhane0 <harshbhange123@gmail.com>
2024-04-08 17:42:10 +0530

    "Add API key validation middleware and use it in route group"
    This diff adds a new middleware function `validateApiKey` which checks if an API key is provided in the request headers and if it matches the server key stored in an environment variable. If the key is missing or does not match, an error response is returned. This middleware is then applied to the route group by using `app.use("/api/v1", validateApiKey, routerG)`.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { ConnectDB } from "./database";
 import { routerG } from "./routes";
 import { data } from "./routs";
 import { ApiResponse } from "./utils/helper";
+import { validateApiKey } from "./middleware";
 const app = express();
 const port = 3000;
 dotenv.config();
@@ -12,7 +13,7 @@ app.get("/", (req: Request, res: Response) => {
   return ApiResponse({ res, data, msg: "server  is running", code: 200 });
 });
 let user = 0;
-app.use("/api/v1", routerG);
+app.use("/api/v1", validateApiKey, routerG);
 ConnectDB();
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import { validateToken } from "../utils/JwtToken";
+import { ApiResponse } from "../utils/helper";
 
 export const validateAdmin = async (
   req: Request,
@@ -21,4 +22,23 @@ export const validateAdmin = async (
       .json({ auth: false, error: "Internal Server Error" })
       .status(500);
   }
+};
+
+export const validateApiKey = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { api_key } = req.headers;
+  const ServerKye = process.env.API_KEY;
+  if (!api_key) {
+    return ApiResponse({ res, msg: "Api Key Missing In Headers", code: 200 });
+  }
+  if (!ServerKye) {
+    return ApiResponse({ res, msg: "Pleas Provide Api Key As Env", code: 200 });
+  }
+  if (api_key !== ServerKye) {
+    return ApiResponse({ res, msg: "Unauthorized Request", code: 200 });
+  }
+  return next();
 };
